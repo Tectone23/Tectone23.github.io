0d6e6e4d30bf6756a2c4c5bd7b0f976a60ba5dea
Abdisamad Hashi <abdisamad@tectone23.com>
2024-03-21 11:35:36 +0000

    fix

--- docs/ci_env.ts
+++ docs/ci_env.ts
@@ -1,5 +1,9 @@
-export const ci_config = {
-  macBuild: process.env.MACOS_BUILD_URL,
-  linuxBuild: process.env.LINUX_BUILD_URL,
-  windowsBuild: process.env.WINDOWS_BUILD_URL,
-};
+// export const ci_config = {
+//   macBuild: process.env.MACOS_BUILD_URL,
+//   linuxBuild: process.env.LINUX_BUILD_URL,
+//   windowsBuild: process.env.WINDOWS_BUILD_URL,
+// };
+
+export const macBuild = process.env.MACOS_BUILD_URL;
+export const windowsBuild = process.env.WINDOWS_BUILD_URL;
+export const linuxBuild = process.env.LINUX_BUILD_URL;
